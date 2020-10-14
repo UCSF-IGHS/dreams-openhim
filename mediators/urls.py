5491a70cfdd47c5102354cfdc0eeaee4485f84e5
David Mugume <david.mugume@ucglobalprograms.org>
2020-10-14 17:47:02 +0300

    Added placeholders for the DREAMS conversion mediator

--- mediators/urls.py
+++ mediators/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register-echo-mediator/', views.RegisterEchoAPIView.as_view()),
     path('echo/', views.EchoAPIView.as_view()),
+    path('register-dreams-odk-mediator/', views.DreamsODKAPIView.as_view()),
     path('dreams-odk/', views.DreamsODKAPIView.as_view()),
 ]
--- mediators/views.py
+++ mediators/views.py
@@ -10,7 +10,19 @@ def index(request):
     return HttpResponse("Hello, world. You're at the him-mediators-app index.")
 
 
+class RegisterEchoAPIView(APIView):
+    def post(self, request):
+        print('Initialize Mediator')
+        EchoMediator()
+        return Response("Working", status.HTTP_200_OK)
+
+
 class EchoAPIView(APIView):
+    def post(self, request):
+        return Response(self.request.data, status.HTTP_200_OK)
+
+
+class RegisterDreamsODKAPIView(APIView):
     def post(self, request):
         print('Initialize Mediator')
         EchoMediator()
@@ -19,4 +31,9 @@ class EchoAPIView(APIView):
 
 class DreamsODKAPIView(APIView):
     def post(self, request):
+        # get DREAMS APP API
+        # change json format
+        # response with the json
         return Response(self.request.data, status.HTTP_200_OK)
+
+# ODK -> OpenHIM -> DreamsODKAPIView -> OpenHIM -> DREAMS app
