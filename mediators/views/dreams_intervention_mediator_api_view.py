54585af842612e6f991a99bff123d060ef80e0aa
David Mugume <david.mugume@ucglobalprograms.org>
2020-10-23 11:13:31 +0300

    HIM-core response Refactored the response object to the HIM-core to have to correct responses as per the orchestrations done. Fixed unit tests

--- mediators/views/dreams_intervention_mediator_api_view.py
+++ mediators/views/dreams_intervention_mediator_api_view.py
@@ -14,29 +14,29 @@ from datetime import datetime
 class DreamsInterventionMediatorAPIView(APIView):
     def post(self, request):
         mediator = DreamsInterventionMediator()
-        request_body = request.body.decode("utf-8")
+        # request_body = request.body.decode("utf-8")
         converted_json = mediator.convert_to_dream_intervention_api_json(self.request.data)
         api_response = self.call_dreams_interventions_api(converted_json)
-        orchestration_results = self.generate_orchestration_results(request, request_body, api_response)
+        orchestration_results = self.generate_orchestration_results(request, api_response)
         response = json.dumps(orchestration_results)
-        return HttpResponse(response, content_type='application/json')
+        return HttpResponse(response, content_type='application/json', status=api_response.status_code)
 
     def call_dreams_interventions_api(self, data):
         api_conf = copy(settings.DREAMS_INTERVENTION_API_ENDPOINT_CONF)
         headers = {"Content-Type": "application/json"}
-        response = requests.post(url=api_conf['api_end_point'], headers=headers,
-                                 data=data, auth=HTTPBasicAuth(api_conf['api_user_name'],
-                                                               api_conf['api_password']))
+        response = requests.post(url=api_conf['api_end_point'], headers=headers, data=data,
+                                 auth=HTTPBasicAuth(api_conf['api_user_name'], api_conf['api_password']))
         return response
 
-    def generate_orchestration_results(self, request, request_body, response):
+    def generate_orchestration_results(self, request, response):
         mediator_conf = copy(settings.DREAMS_INTERVENTION_MEDIATOR_CONF)
         timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
         orchestrations_results = [{
             "name": "Post DREAMS Intervention",
             "request": {
                 "path": response.request.path_url,
-                "headers": response.request.headers._store,
+                "headers": dict(response.request.headers),
                 "querystring": None,
                 "body": json.loads(response.request.body),
                 "method": response.request.method,
@@ -49,23 +49,42 @@ class DreamsInterventionMediatorAPIView(APIView):
             }
         }]
 
-        openhim_response = {
-            "status": response.status_code,
+        properties = {
+            "interventions_count": len(request.data)
+        }
+
+        response_status = {
+            "processing": 'Processing',
+            "failed": 'Failed',
+            "completed": 'Completed',
+            "successful": 'Successful',
+            "with_errors": 'Completed with error(s)'
+        }
+        status = ''
+        if 100 <= response.status_code <= 199:
+            status = response_status['processing']
+        elif 200 <= response.status_code <= 299:
+            status = response_status['successful']
+        elif 300 <= response.status_code <= 399:
+            status = response_status['completed']
+        elif 400 <= response.status_code <= 499:
+            status = response_status['completed']
+        elif 500 <= response.status_code <= 599:
+            status = response_status['failed']
+
+        response_to_originating_client = {
+            "status": 200,
             "headers": {
                 "content-type": "application/json"
             },
-            "body": json.loads(request.body),
+            "body": request.data,
             "timestamp": timestamp
-        }
-
-        properties = {
-            "interventions_count": len(json.loads(request.body))
         }
 
         return_object = {
             "x-mediator-urn": mediator_conf['urn'],
-            "status": response.status_code,
-            "response": openhim_response,
+            "status": status,
+            "response": response_to_originating_client,
             "orchestrations": orchestrations_results,
             "properties": properties
         }
